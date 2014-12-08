ba08f983d94349436cb2f6b26aed5bf3083c3eec
Pawel Miniszewski <venetian5@gmail.com>
2014-12-08 21:12:16 +0100

    - changed bullet sprite texture

--- src/Entities/Bullet.cpp
+++ src/Entities/Bullet.cpp
@@ -1,8 +1,8 @@
 #include "Bullet.h"
-
+#include "ResourceCache.h"
 Bullet::Bullet(sf::Vector2f dir): dir(dir)
 {
-
+    SetTexture(ResourceCache::LoadTexture("assets/particle.png"));
 }
 
 void Bullet::Update(float dt)
